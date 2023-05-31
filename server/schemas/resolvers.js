6ce6bebfa45f3c92358ec7455e1c60d9f075a856
juella205 <johnsonjulie455@gmail.com>
2023-05-31 19:29:33 -0400

    complete the creating a user mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,18 @@ const resolvers = {
       createUser: async (parent, { input }, context) => {
         // Perform validation, password hashing, and database insertion
         const { name, email, password } = input;
-        
+        const user = await User.create({
+            name,
+            email,
+            password, // Remember to hash the password before storing it in the database
+          });
+    
+          return user;
+        },
+        // Add other mutation resolvers here if needed
+      },
+    };
+    
+    module.exports = resolvers;
         
   
