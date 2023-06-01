f3788fed275754495b84d34a0bf03f5bbb3386c6
DenimB96 <rlsjr2@live.com>
2023-05-31 21:16:52 -0400

    Added schema for activity model, then updated user.js to have activities included with the user and made a method to be able to get all user activities for a particular city, added queries and mutations to retrieve, create, update, and delete activities and update typeDefs to reflect changes in models and resolvers.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,7 @@ const typeDefs = gql`
     email: String
     password: String
     cities: [City]!
+    activities: [Activity]!
   }
 
   type City {
@@ -17,8 +18,9 @@ const typeDefs = gql`
 
   type Activity {
     _id: ID
-    activityText: String
-    activityCity: String
+    title: String
+    description: String
+    city: String
     createdAt: String
   }
 
@@ -29,12 +31,19 @@ const typeDefs = gql`
 
   type Query {
     users: [User]
+    userActivities(userId: ID!, city: String!): [Activity]
   }
 
   type Mutation {
-
+    createActivity(title: String, description: String): Activity
+    updateActivity(id: ID!, title: String, description: String): Activity
+    deleteActivity(id: ID!): Activity
   }
 `;
 
 
 module.exports = typeDefs;
+// Added Activities field to User type to represent the users activities
+// Added userActivities to query type which accepts userId and city as args and returns an array of activities
+// Added create, update, and delete activity mutations to handle each respectively
+// Changed Activity type fields to match the activity model
