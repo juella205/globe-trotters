9c46a388dac9362495e130e4d740f4f1d6604a65
juella205 <johnsonjulie455@gmail.com>
2023-05-31 20:15:15 -0400

    added Authentication error for the login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,38 @@
 const jwt = require('jsonwebtoken');
-cconst resolvers = {
-    Query: {
-      users: async () => {
-        return User.find().populate('thoughts');
-      },
-    Mutation: {
-      createUser: async (parent, { input }, context) => {
-        // Create user
-     const { name, email, password } = input;
-    const user = await User.create({ name,email,password,});
-     const token = signToken(user);
-          return user;
-        },
-        // Add other mutation resolvers here if needed
-        login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-      },
-    }; 
-    
-    module.exports = resolvers;
-    };
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate('thoughts');
+    },
+  },
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      // Create user
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return {token, user };
+    },
+    // Add other mutation resolvers here if needed
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      // If there is no user with the email address, return an Authentication error stating sp
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+      
+
+
+
+
+    },
+  },
 };
+
+module.exports = resolvers;
+
+    
+
 
   
