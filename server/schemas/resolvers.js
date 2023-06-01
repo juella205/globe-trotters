5b5f01653861e0c9b5380dbe5b32cfe3f5f63ee7
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-05-31 17:56:16 -0700

    add queries and mutations for cities

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,32 @@
+// insert code from Stephen
+
+const resolvers = {
+    Query: {
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('cities');
+        },
+        cities: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return City.find(params).sort({ createdAt: -1 });
+        },
+        city: async (parent, { cityId }) => {
+            return City.findOne({ _id: cityId });
+        },
+    },
+
+    Mutation: {
+        addCity: async (parent, { cityName, username }) => {
+            const city = await City.create({ cityName, username });
+
+            await User.findOneAndUpdate(
+                { username: username },
+                { $addToSet: { cities: city._id } }
+            );
+
+            return city;
+        },
+        removeCity: async (parent, { cityId }) => {
+            return City.findOneAndDelete({ _id: cityId });
+        },
+    }
+};
