2a7fb569e1ebb0c40565594825e50fff4edef166
juella205 <johnsonjulie455@gmail.com>
2023-06-05 19:31:51 -0400

    created a user model

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,42 @@ const User = require('./User');
 const City = require('./City');
 const Activity = require('./Activity');
 
+const mongoose = require('mongoose');
+const url = 'mongodb://127.0.0.1:27017/globetrotters-guide';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected successfully to the database');
+    saveUser();
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error);
+  });
+  const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = { User, City, Activity };
